36b95f1b2c161a2df51d3bdaec0c8b96c02df801
Benjamin Kowarsch <trijezdci@users.noreply.github.com>
2023-04-29 00:12:45 +0900

    new file

---
+++ src/m2-build-params.h
@@ -0,0 +1,27 @@
+/* m2-build-params.h -- build parameters for M2C -- public domain file */
+
+#ifndef M2C_BUILD_PARAMS_H
+#define M2C_BUILD_PARAMS_H
+
+/* infile parameters */
+
+#define M2C_MAX_INFILE_SIZE 65536
+#define M2C_MAX_INFILE_LINES 12000
+#define M2C_MAX_INFILE_COLUMNS 160
+
+/* lexical parameters */
+
+#define M2C_MAX_IDENT_LENGTH 64
+#define M2C_MAX_NUMBER_LENGTH 32
+#define M2C_MAX_STRING_LENGTH 160
+#define M2C_MAX_COMMENT_LENGTH 4096
+
+/* code generation parameters */
+
+#define M2C_MAX_C_MACRO_LENGTH 64
+#define M2C_MAX_C_IDENT_LENGTH 64
+
+
+#endif /* M2C_BUILD_PARAMS_H */
+
+/* END OF FILE */
