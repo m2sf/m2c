d5dbd437c088abb0f74fa2d067c3722b6fa63ae2
Benjamin Kowarsch <trijezdci@users.noreply.github.com>
2023-05-03 03:03:21 +0900

    formatting

--- src/lib/io/basic-fileio.h
+++ src/lib/io/basic-fileio.h
@@ -36,7 +36,7 @@
  * You should have received  a copy of the GNU Lesser General Public License *
  * along with M2C.  If not, see <https://www.gnu.org/copyleft/lesser.html>.  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
- 
+
 #ifndef FILEIO_H
 #define FILEIO_H
 
