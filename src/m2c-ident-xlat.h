a5630e80a8e2ec67c258bb5e3700a42f6f808779
Benjamin Kowarsch <trijezdci@users.noreply.github.com>
2023-05-04 03:30:41 +0900

    new file

---
+++ src/m2c-ident-xlat.h
@@ -0,0 +1,107 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * M2C Modula-2 Compiler & Translator                                        *
+ *                                                                           *
+ * Copyright (c) 2015-2023 Benjamin Kowarsch                                 *
+ *                                                                           *
+ * @synopsis                                                                 *
+ *                                                                           *
+ * M2C is a portable  Modula-2 to C translator  and  via-C compiler  for the *
+ * bootstrap subset of the revised Modula-2 language described in            *
+ *                                                                           *
+ * https://github.com/m2sf/m2bsk/wiki/Language-Specification                 *
+ *                                                                           *
+ * In translator mode,  M2C translates Modula-2 source files to semantically *
+ * equivalent C source files.  In compiler mode,  it translates the Modula-2 *
+ * source files  to C,  then compiles the resulting C sources  to object and *
+ * executable files using the host system's resident C compiler and linker.  *
+ *                                                                           *
+ * Further information at https://github.com/m2sf/m2c/wiki                   *
+ *                                                                           *
+ * @file                                                                     *
+ *                                                                           *
+ * m2c-ident-xlat.h                                                          *
+ *                                                                           *
+ * Public interface of identifier translation module.                        *
+ *                                                                           *
+ * @license                                                                  *
+ *                                                                           *
+ * M2C is free software:  You can redistribute and modify it under the terms *
+ * of the GNU Lesser General Public License (LGPL)  either version 2.1 or at *
+ * your choice version 3, both published by the Free Software Foundation.    *
+ *                                                                           *
+ * M2C is distributed in the hope it may be useful, but strictly WITHOUT ANY *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS *
+ * FOR ANY PARTICULAR PURPOSE.  Read the license for more details.           *
+ *                                                                           *
+ * You should have received  a copy of the GNU Lesser General Public License *
+ * along with M2C.  If not, see <https://www.gnu.org/copyleft/lesser.html>.  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef M2C_IDENT_XLAT_H
+#define M2C_IDENT_XLAT_H
+
+/* --------------------------------------------------------------------------
+ * type m2c_ident_xlat_kind_t
+ * --------------------------------------------------------------------------
+ * Categories of identifiers.
+ * ----------------------------------------------------------------------- */
+
+typedef enum {
+    M2C_IDENT_XLAT_KIND_CONST,
+    M2C_IDENT_XLAT_KIND_TYPE,
+    M2C_IDENT_XLAT_KIND_VAR,
+    M2C_IDENT_XLAT_KIND_FUNC,
+    M2C_IDENT_XLAT_KIND_PROC,
+} m2c_ident_xlat_kind_t;
+
+
+/* --------------------------------------------------------------------------
+ * function m2c_ident_xlat_import_guard(module_id)
+ * --------------------------------------------------------------------------
+ * Returns an import guard C macro identifier for module_id. 
+ * ----------------------------------------------------------------------- */
+
+const char* m2c_ident_xlat_import_guard (intstr_t module_id);
+
+
+/* --------------------------------------------------------------------------
+ * function m2c_ident_xlat_for_exported_name(type, module_id, enum_id, ident)
+ * --------------------------------------------------------------------------
+ * Returns a fully qualified C identifier for module_id and ident. 
+ * ----------------------------------------------------------------------- */
+
+const char* m2c_ident_xlat_for_exported_name
+  (m2c_ident_xlat_kind_t kind,
+   intstr_t module_id, /* required */
+   intstr_t enum_id,   /* may be NULL */
+   intstr_t ident);    /* required */
+
+
+/* --------------------------------------------------------------------------
+ * function m2c_ident_xlat_for_hidden_name(type, enum_id, ident)
+ * --------------------------------------------------------------------------
+ * Returns a file level C identifier for ident. 
+ * ----------------------------------------------------------------------- */
+
+const char* m2c_ident_xlat_for_hidden_name
+  (m2c_ident_xlat_kind_t kind,
+   intstr_t enum_id,   /* may be NULL */
+   intstr_t ident);    /* required */
+
+
+/* --------------------------------------------------------------------------
+ * function m2c_ident_xlat_for_local_name(type, proc_id, enum_id, ident)
+ * --------------------------------------------------------------------------
+ * Returns a local C identifier from proc_id for ident.
+ * ----------------------------------------------------------------------- */
+
+const char* m2c_ident_xlat_for_local_name
+  (m2c_ident_xlat_kind_t kind,
+   intstr_t proc_id,   /* required */
+   intstr_t enum_id,   /* may be NULL */
+   intstr_t ident);    /* required */
+
+
+#endif /* M2C_IDENT_XLAT_H */
+
+/* END OF FILE */
