4e30521b27173740459cd6c3f3cf16d01d0063ff
Benjamin Kowarsch <trijezdci@gmail.com>
2023-05-21 00:26:53 +0900

    work in progress

---
+++ src/imp/m2c-first-sets.c
@@ -0,0 +1,76 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * M2C Modula-2 Compiler & Translator                                        *
+ *                                                                           *
+ * Copyright (c) 2015-2023 Benjamin Kowarsch                                 *
+ *                                                                           *
+ * @synopsis                                                                 *
+ *                                                                           *
+ * M2C is a portable  Modula-2 to C translator  and  via-C compiler  for the *
+ * bootstrap subset of the revised Modula-2 language described in            *
+ *                                                                           *
+ * https://github.com/m2sf/m2bsk/wiki/Language-Specification                 *
+ *                                                                           *
+ * In translator mode,  M2C translates Modula-2 source files to semantically *
+ * equivalent C source files.  In compiler mode,  it translates the Modula-2 *
+ * source files  to C,  then compiles the resulting C sources  to object and *
+ * executable files using the host system's resident C compiler and linker.  *
+ *                                                                           *
+ * Further information at https://github.com/m2sf/m2c/wiki                   *
+ *                                                                           *
+ * @file                                                                     *
+ *                                                                           *
+ * m2c-first-sets.c                                                          *
+ *                                                                           *
+ * Implementation of FIRST sets module.                                      *
+ *                                                                           *
+ * @license                                                                  *
+ *                                                                           *
+ * M2C is free software:  You can redistribute and modify it under the terms *
+ * of the GNU Lesser General Public License (LGPL)  either version 2.1 or at *
+ * your choice version 3, both published by the Free Software Foundation.    *
+ *                                                                           *
+ * M2C is distributed in the hope it may be useful, but strictly WITHOUT ANY *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS *
+ * FOR ANY PARTICULAR PURPOSE.  Read the license for more details.           *
+ *                                                                           *
+ * You should have received  a copy of the GNU Lesser General Public License *
+ * along with M2C.  If not, see <https://www.gnu.org/copyleft/lesser.html>.  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+/* --------------------------------------------------------------------------
+ * imports
+ * ----------------------------------------------------------------------- */
+
+#include "m2c-productions.h"
+
+
+/* --------------------------------------------------------------------------
+ * FIRST sets
+ * ----------------------------------------------------------------------- */
+
+static const m2c_tokenset_t first_set[] = {
+#include "m2c-first-set-literals.h"
+}; /* end first_set */
+
+
+/* --------------------------------------------------------------------------
+ * function FIRST(production)
+ * --------------------------------------------------------------------------
+ * Returns the FIRST set for production p,  or NULL if p is invalid.
+ * ----------------------------------------------------------------------- */
+
+m2c_tokenset_t FIRST (m2c_production_t p) {
+ 
+ if (IS_VALID_PRODUCTION(p)) {
+   return &first_set[p];
+ }
+ else /* invalid production */ {
+   return NULL;
+ } /* end if */
+} /* end FIRST */
+
+
+#endif /* M2C_FIRST_SETS_H */
+
+/* END OF FILE */
---
+++ src/m2c-first-sets.h
@@ -0,0 +1,62 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * M2C Modula-2 Compiler & Translator                                        *
+ *                                                                           *
+ * Copyright (c) 2015-2023 Benjamin Kowarsch                                 *
+ *                                                                           *
+ * @synopsis                                                                 *
+ *                                                                           *
+ * M2C is a portable  Modula-2 to C translator  and  via-C compiler  for the *
+ * bootstrap subset of the revised Modula-2 language described in            *
+ *                                                                           *
+ * https://github.com/m2sf/m2bsk/wiki/Language-Specification                 *
+ *                                                                           *
+ * In translator mode,  M2C translates Modula-2 source files to semantically *
+ * equivalent C source files.  In compiler mode,  it translates the Modula-2 *
+ * source files  to C,  then compiles the resulting C sources  to object and *
+ * executable files using the host system's resident C compiler and linker.  *
+ *                                                                           *
+ * Further information at https://github.com/m2sf/m2c/wiki                   *
+ *                                                                           *
+ * @file                                                                     *
+ *                                                                           *
+ * m2c-first-sets.h                                                          *
+ *                                                                           *
+ * Public interface for FIRST sets module.                                   *
+ *                                                                           *
+ * @license                                                                  *
+ *                                                                           *
+ * M2C is free software:  You can redistribute and modify it under the terms *
+ * of the GNU Lesser General Public License (LGPL)  either version 2.1 or at *
+ * your choice version 3, both published by the Free Software Foundation.    *
+ *                                                                           *
+ * M2C is distributed in the hope it may be useful, but strictly WITHOUT ANY *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS *
+ * FOR ANY PARTICULAR PURPOSE.  Read the license for more details.           *
+ *                                                                           *
+ * You should have received  a copy of the GNU Lesser General Public License *
+ * along with M2C.  If not, see <https://www.gnu.org/copyleft/lesser.html>.  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef M2C_FIRST_SETS_H
+#define M2C_FIRST_SETS_H
+
+/* --------------------------------------------------------------------------
+ * imports
+ * ----------------------------------------------------------------------- */
+
+#include "m2c-token.h"
+#include "m2c-productions.h"
+
+
+/* --------------------------------------------------------------------------
+ * function FIRST(production)
+ * --------------------------------------------------------------------------
+ * Returns the FIRST set for production p,  or NULL if p is invalid.
+ * ----------------------------------------------------------------------- */
+
+m2c_tokenset_t FIRST (m2c_production_t p);
+
+
+#endif /* M2C_FIRST_SETS_H */
+
+/* END OF FILE */
